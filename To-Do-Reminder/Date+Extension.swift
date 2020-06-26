b4675ccc68884f008140984393e4b26483742e0a
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-06-26 14:59:28 -0400

    Date component to notify function added in date extension file

--- To-Do-Reminder/Date+Extension.swift
+++ To-Do-Reminder/Date+Extension.swift
@@ -16,3 +16,15 @@ extension Date {
         return components
     }
    
+ 
+    func dateComponentsToNotify() -> DateComponents {
+        let calendar = Calendar.current
+        let newDate = calendar.date(byAdding: .minute, value: -30, to: self)
+        guard let date = newDate else {
+            return calendar.dateComponents([.year, .month, .day, .hour, .minute], from: self)
+        }
+        let components = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+        return components
+    }
+    
+    
