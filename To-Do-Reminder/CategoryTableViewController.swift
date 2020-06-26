ee5596def5a5a5a09882071dbf5afc2a5c97c66b
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-06-26 18:17:27 -0400

    Category array to populate list

---
+++ To-Do-Reminder/CategoryTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryTableViewController.swift
+//  To-Do-Reminder
+//
+//  Created by Kritima Kukreja on 2020-06-26.
+//  Copyright © 2020 Kritima Kukreja. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryTableViewController: UITableViewController {
+    
+    // create a folder array to populate the table
+    var categories = [Categories]()
+    
+    
