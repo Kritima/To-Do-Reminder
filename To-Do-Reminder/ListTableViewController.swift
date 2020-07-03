d19e441720be5cd4be1d8cc838d5b81ff64b87f8
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-07-03 19:06:31 -0400

    List Table view controller fixed

--- To-Do-Reminder/ListTableViewController.swift
+++ To-Do-Reminder/ListTableViewController.swift
@@ -9,117 +9,281 @@
 import UIKit
 import CoreData
 
-class ListTableViewController: UITableViewController {
+class ListTableViewController: UIViewController {
 
-   /* // create a folder array to populate the table
-    var lists = []()
+    var selectedSort = 0
+       var selectedCategory: Categories? {
+           didSet {
+               loadTodos()
+           }
+       }
+       
     
-    // create a context
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
-        
-        loadFolder()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        tableView.reloadData()
-    }
-    
-    //MARK: - Data manipulation methods
-    
-    func loadFolder() {
-        let request: NSFetchRequest<Lists> = Categories.fetchRequest()
-        
-        do {
-            categories = try context.fetch(request)
-        } catch {
-            print("Error loading categories \(error.localizedDescription)")
-        }
-        
-        tableView.reloadData()
-    }
-    
-    func saveFolders() {
-        do {
-            try context.save()
-            tableView.reloadData()
-        } catch {
-            print("Error saving folders \(error.localizedDescription)")
-        }
-    }
-    
-    // MARK: - Table view data source
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return lists.count
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
-        cell.textLabel?.text = lists[indexPath.row].category
-     //   cell.textLabel?.textColor = .lightGray
-     //   cell.detailTextLabel?.textColor = .lightGray
-        //  cell.detailTextLabel?.text = "\(categories[indexPath.row].notes?.count ?? 0)"
-        cell.imageView?.image = UIImage(systemName: "folder")
-        cell.selectionStyle = .none
-        return cell
-    }
-    
-    //MARK: - add category method
-    @IBAction func addFolder(_ sender: UIBarButtonItem) {
-        
-        var textField = UITextField()
-        let alert = UIAlertController(title: "Add New List", message: "", preferredStyle: .alert)
-        let addAction = UIAlertAction(title: "Add", style: .default) { (action) in
-            let folderNames = self.categories.map {$0.category}
-            guard !folderNames.contains(textField.text) else {self.showAlert(); return}
-            let newFolder = Categories(context: self.context)
-            newFolder.category = textField.text!
-            self.categories.append(newFolder)
-            self.saveFolders()
-        }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        // change the font color of cancel action
-        cancelAction.setValue(UIColor.orange, forKey: "titleTextColor")
-        
-        alert.addAction(addAction)
-        alert.addAction(cancelAction)
-        alert.addTextField { (field) in
-            textField = field
-            textField.placeholder = "Folder Name"
-        }
-        
-        present(alert, animated: true, completion: nil)
-    }
-    
-    func showAlert() {
-        let alert = UIAlertController(title: "Name Taken", message: "Please choose another name", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        okAction.setValue(UIColor.orange, forKey: "titleTextColor")
-        alert.addAction(okAction)
-        present(alert, animated: true, completion: nil)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let destination = segue.destination as! ListTableViewController
-        if let indexPath = tableView.indexPathForSelectedRow {
-            destination.selectedFolder = categories[indexPath.row]
-        }
-        
-    }
-}*/
-}
+    var categoryName: String!
+       let todoListContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+       var tasksArray = [Todo]()
+       var selectedTodo: Todo?
+       var todoToMove = [Todo]()
+       
+       let searchController = UISearchController(searchResultsController: nil)
+       
+       
+        @IBOutlet var tableView: UITableView!
+       @IBOutlet weak var categoryLabel: UILabel!
+       @IBOutlet weak var sortSegment: UISegmentedControl!
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           setUpTableView()
+           showSearchBar()
+           categoryLabel.text = selectedCategory!.category
+           tableView.tableFooterView = UIView()
+       }
+       
+       //Moving to add tasks screen
+       @IBAction func addTodo(_ sender: Any) {
+           performSegue(withIdentifier: "todoViewScreen", sender: self)
+       }
+       
+       //Integer to indicate the type of sort
+       @IBAction func sortTodos(_ sender: UISegmentedControl)
+       {
+           switch sender.selectedSegmentIndex
+           {
+           case 0: selectedSort = 0
+               break
+           case 1: selectedSort = 1
+               break
+           default:
+               break
+           }
+           loadTodos()
+           tableView.reloadData()
+       }
+       
+       override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+           
+           if let destination = segue.destination as? ListDetailViewController
+           {
+               destination.delegate = self
+               if selectedTodo != nil
+               {
+                   destination.todo = selectedTodo
+               }
+           }
+           
+           if let destination = segue.destination as? MoveToFolderViewController
+           {
+                   destination.selectedNote = todoToMove
+           }
+       }
+       
+       @IBAction func unwindToTaskListView(_ unwindSegue: UIStoryboardSegue) {
+           saveTodos()
+           loadTodos()
+           tableView.reloadData()
+       }
+   }
 
+
+   extension ListTableViewController {
+       
+       func loadTodos(with request: NSFetchRequest<Todo> = Todo.fetchRequest(), predicate: NSPredicate? = nil) {
+           
+           let sortOptions = ["date", "name"]
+           let todoPredicate = NSPredicate(format: "parentFolder.name=%@", selectedCategory!.category!)
+           request.sortDescriptors = [NSSortDescriptor(key: sortOptions[selectedSort], ascending: true)]
+           if let addtionalPredicate = predicate {
+               request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [todoPredicate, addtionalPredicate])
+           } else {
+               request.predicate = todoPredicate
+           }
+           
+           do {
+               tasksArray = try todoListContext.fetch(request)
+           } catch {
+               print("Error loading todos \(error.localizedDescription)")
+           }
+           
+       }
+       
+       func deleteTodoFromList() {
+           
+           todoListContext.delete(selectedTodo!)
+           tasksArray.removeAll { (Todo) -> Bool in
+               Todo == selectedTodo!
+           }
+           tableView.reloadData()
+           
+       }
+       
+       
+       func saveTodos()
+       {
+           do {
+               try todoListContext.save()
+           } catch {
+               print("Error saving the context \(error.localizedDescription)")
+           }
+       }
+       
+       func updateTodo() {
+           saveTodos()
+           tableView.reloadData()
+       }
+       
     
-  
+       func markTodoCompleted() {
+           
+           let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+           let request: NSFetchRequest<Categories> = Categories.fetchRequest()
+           let folderPredicate = NSPredicate(format: "name MATCHES %@", "Archived")
+           request.predicate = folderPredicate
+           do {
+               let category = try context.fetch(request)
+               self.selectedTodo?.parentFolder = category.first
+               saveTodos()
+               tasksArray.removeAll { (Todo) -> Bool in
+                   Todo == selectedTodo!
+               }
+               tableView.reloadData()
+           } catch {
+               print("Error fetching data \(error.localizedDescription)")
+           }
+           
+       }
+       
+       func saveTodo(title: String, taskText: String, dueDate: Date)
+       {
+           let todo = Todo(context: todoListContext)
+           todo.title = title
+           todo.text = taskText
+           todo.due_date = dueDate
+           todo.date = Date()
+           todo.parentFolder = selectedCategory
+           saveTodos()
+           tasksArray.append(todo)
+           tableView.reloadData()
+       }
+       
+       
+       override func viewWillAppear(_ animated: Bool) {
+           selectedTodo = nil
+       }
+       
+   }
+
+
+   extension ListTableViewController: UITableViewDelegate, UITableViewDataSource {
+       func setUpTableView() {
+           tableView.delegate = self
+           tableView.dataSource = self
+        
+           tableView.estimatedRowHeight = 44
+           tableView.rowHeight = UITableView.automaticDimension
+       }
+       
+       
+       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return tasksArray.count
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+           let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+           let task = tasksArray[indexPath.row]
+           cell.textLabel?.text = task.title
+           
+       
+           if (task.due_date! < Date() && task.parentFolder?.category != "Archived")
+           {
+               cell.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+           }
+
+           if (Calendar.current.isDateInToday(task.due_date!) && task.parentFolder?.category != "Archived")
+           {
+               cell.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+           }
+           return cell
+       }
+       
+       func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+           let delete = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completion) in
+               
+               self.todoListContext.delete(self.tasksArray[indexPath.row])
+               self.tasksArray.remove(at: indexPath.row)
+               tableView.deleteRows(at: [indexPath], with: .fade)
+               completion(true)
+           }
+           
+           delete.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+           delete.image = UIImage(systemName: "trash.fill")
+           return UISwipeActionsConfiguration(actions: [delete])
+       }
+       
+       func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+           let complete = UIContextualAction(style: .normal, title: "Done") { (action, view, completion) in
+               self.selectedTodo = self.tasksArray[indexPath.row]
+               self.markTodoCompleted()
+           }
+           let move = UIContextualAction(style: .normal, title: "Move") { (action, view, completion) in
+               self.todoToMove.append(self.tasksArray[indexPath.row])
+               self.performSegue(withIdentifier: "moveTodoScreen", sender: nil)
+           }
+           complete.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+           complete.image = UIImage(systemName: "checkmark.circle")
+           move.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+           move.image = UIImage(systemName: "arrowshape.turn.up.right")
+           return UISwipeActionsConfiguration(actions: [complete, move])
+       }
+       
+       func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+           selectedTodo = tasksArray[indexPath.row]
+           performSegue(withIdentifier: "todoViewScreen", sender: self)
+       }
+   }
+
+   extension ListTableViewController: UISearchBarDelegate {
+       
+       func showSearchBar()
+       {
+           searchController.obscuresBackgroundDuringPresentation = false
+           searchController.searchBar.placeholder = "Search Folder"
+           navigationItem.searchController = searchController
+           searchController.searchBar.delegate = self
+           definesPresentationContext = true
+           searchController.searchBar.searchTextField.textColor = .white
+       }
+       
+       func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
+           
+       {
+           let predicate = NSPredicate(format: "name CONTAINS[cd] %@", searchBar.text!)
+           loadTodos(predicate: predicate)
+           tableView.reloadData()
+       }
+       
+       func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+           if searchBar.text?.count == 0 {
+               loadTodos()
+
+               DispatchQueue.main.async {
+                   searchBar.resignFirstResponder()
+               }
+
+           }
+           loadTodos()
+           tableView.reloadData()
+       }
+       
+       
+       
+       func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+           loadTodos()
+           DispatchQueue.main.async {
+               searchBar.resignFirstResponder()
+           }
+           tableView.reloadData()
+           searchBar.resignFirstResponder()
+       }
+   }
+
